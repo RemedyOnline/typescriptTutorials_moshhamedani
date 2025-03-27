0cf480384ca477bbe2464b78c9f418ecf1d5f09c
Mike <agyemangmichael555@gmail.com>
2025-03-27 22:57:34 +0000

    enjoying Mosh Hamedani's Lessons on Typescript

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+console.log("Hi TypeScript");
+console.log("This is my second Tutorial on you bro!");
+console.log("I'm here to murder you, niqqa!!!!!!!!!");
+
+let age: number = 15;
+console.log(`My age is ${age}`);
+
+if (age < 50) age += 10;
+console.log(age);
